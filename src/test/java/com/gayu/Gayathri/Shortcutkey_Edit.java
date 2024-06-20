c7fee13c007501942fc16c0fcab2b777ff2a8736
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2024-06-20 13:32:59 +0530

    Shortcutkey_Edit.java

--- src/test/java/com/gayu/Gayathri/Shortcutkey_Edit.java
+++ src/test/java/com/gayu/Gayathri/Shortcutkey_Edit.java
@@ -23,7 +23,7 @@ public class Shortcutkey_Edit {
 	@BeforeTest
 	public void setup() throws MalformedURLException {
 		DesiredCapabilities dc = DesiredCapabilities.chrome();
-		URL url = new URL("http://172.12.20.99:4443/wd/hub");
+		URL url = new URL("http://172.20.23.92:4443/wd/hub");
 		driver = new RemoteWebDriver(url, dc);
 	}
 
